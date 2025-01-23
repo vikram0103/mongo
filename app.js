d0f9fbb828950a85a924512b34a74a80ef510cc1
virkam0103 <vikram.s2023cse@sece.ac.in>
2025-01-23 09:45:02 +0530

    atlas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { v4 } = require("uuid");
 
 const app = express();
 app.use(express.json());
-mongoose.connect("mongodb+srv://vikram:vikramrohan@cluster123.3gvvq.mongodb.net/").then(() => {
+mongoose.connect("mongodb+srv://vikram:vikramrohan@mycluster.1ut8r.mongodb.net//expenses").then(() => {
   console.log("connected to MongoDB");
 });
 const expensesSchema = new mongoose.Schema({
